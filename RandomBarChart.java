7601d750df3cc16ee8c29f8f8ceb3b0938ede54d
jorkopp <jorkopp@gmail.com>
2024-09-13 16:00:43 -0700

    Add Lab 3 Source Code

---
+++ RandomBarChart.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class RandomBarChart {
+    public static void main(String[] args) throws Exception {
+        JFrame frame = new JFrame(title:"Random Bar Chart");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(null);
+    }
+}
